e6522e32b4378fa206662ab6a4baaadc2d12448d
sanjay <sanjay7435parmar@gmail.com>
2023-01-28 17:59:36 +0530

    pandey activity_book_now ka layout thik karde

--- app/src/main/java/com/codinglegend/legendsp/cabme/activity_book_now.java
+++ app/src/main/java/com/codinglegend/legendsp/cabme/activity_book_now.java
@@ -1,12 +1,17 @@
 package com.codinglegend.legendsp.cabme;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -33,6 +38,10 @@ public class activity_book_now extends AppCompatActivity {
     String cabId;
 
     String fetchCabDetailsApi = common.getBaseUrl() + "FetchCabDetails.php";
+    String sendCabRequestApi = common.getBaseUrl() + "RequestCab.php";
+
+    Intent mServiceIntent;
+    private YourService mYourService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +54,97 @@ public class activity_book_now extends AppCompatActivity {
 
         getCabDetails();
 
+        mYourService = new YourService();
+        mServiceIntent = new Intent(this, mYourService.getClass());
+        if (!isMyServiceRunning(mYourService.getClass())) {
+            startService(mServiceIntent);
+        }
+
+        binding.doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+                builder1.setMessage("Are you sure?");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                sendCabRequest();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
+
     }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i ("tag", "Running");
+                return true;
+            }
+        }
+        Log.i ("tag", "Not running");
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        //stopService(mServiceIntent);
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction("restartservice");
+        broadcastIntent.setClass(this, Restarter.class);
+        this.sendBroadcast(broadcastIntent);
+        super.onDestroy();
+    }
+
+    private void sendCabRequest() {
+
+        StringRequest request = new StringRequest(Request.Method.POST, sendCabRequestApi,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(context, response, Toast.LENGTH_SHORT).show();
+                                            }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "connection error", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+
+                map.put("cab_id", cabId);
+                map.put("username", common.getUserName(context));
+                map.put("status", "Request");
+
+                return  map;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.add(request);
+
+    }
+
+
 
     private void getCabDetails() {
 
